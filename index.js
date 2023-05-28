17bcacd5eee489ba14345f25f6f42f6454439bed
GreenyDev <joeysamy21308@gmail.com>
2023-05-27 22:49:23 -0400

    change default port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import routes from "./routes/routes.js"
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const port = 6003;
+const port = 5000;
 const app = express();
 
 
